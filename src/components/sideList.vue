94212a8122815185ebd7a36fb265aa3d385ec9f2
young2too <32916647+young2too@users.noreply.github.com>
2020-07-30 15:56:27 +0900

    카카오맵 사이드리스트
    카카오맵 사이드리스트

---
+++ src/components/sideList.vue
@@ -0,0 +1,51 @@
+<template>
+	<div id="sideListWrapper">
+		<div class="sideList">
+		<ul>
+			<li v-for="item in shops">
+				{{ item.FoodName }}
+			</li>
+		</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+import shopList from "../FoodData.json";
+
+export default {
+	props:{
+		sort:{//선택한 음식 종
+			type:String,
+			default:''
+		}
+	},
+	created(){
+		var returnList =[];
+		var addressList = [];
+		for(var i=0;i<shopList.length;i++){
+			if(shopList[i].FoodSort == this.sort){
+				returnList.push(shopList[i]);
+				addressList.push(shopList[i].FoodAddress);
+			}
+		}
+		localStorage.setItem('addresses',addressList);
+		this.shops=returnList;
+	}
+}
+</script>
+
+<style scoped>
+#sideListWrapper{
+	float:left;
+}
+
+li{
+	list-style:none;
+	padding:5px;
+}
+
+li:hover{
+	cursor:pointer;
+}
+</style>
