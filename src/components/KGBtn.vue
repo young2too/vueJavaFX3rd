63bda9774329e60ef9b5fa4ce38aed9fbf7d2201
JungDongJun <67408998+JungDongJun@users.noreply.github.com>
2020-07-30 16:01:46 +0900

    kg버튼
    kg버튼

---
+++ src/components/KGBtn.vue
@@ -0,0 +1,34 @@
+<template>
+	<div id="KGBtnWrapper">
+		<router-link to="/"><img id="KGImg" src='../assets/menu_img/logo.png' v-on:click="gohome"></router-link>
+	</div>
+</template>
+
+<script>
+export default {
+	methods:{
+		gohome(event) {
+    		console.log("홈버튼");
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+#KGImg{
+	width:100px;
+	height:40px;
+}
+#KGImg:hover{
+	transition: none;
+	transform : none;
+}
+
+#KGBtnWrapper{
+	display: inline;
+	margin:0;
+	padding:0;
+}
+
+</style>
