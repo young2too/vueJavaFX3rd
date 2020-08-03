bd3b58a62f56414b867dbe7933243580472d7f76
yyjooooo <67408847+yyjooooo@users.noreply.github.com>
2020-08-03 19:18:51 +0900

    Korea vue

---
+++ src/components/Korea.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="Korea">
+    	<h1> 아름다운   <br> 밥이에요~ </h1>
+		<img id="riceImg" src="../assets/menu_Img/rice123.png">
+	</div>
+</template>
+
+<script>
+export default {
+	data(){
+	},
+	methods: {
+	}
+}
+</script>
+
+<style scoped>
+
+.Korea{
+	text-align: left;
+	width:100%;
+	height:100%;
+	position:absolute;
+	background-color: #f2c84b;
+}
+
+
+#riceImg{
+	max-width: 100%;
+	padding-left: 81.75%;
+	margin-bottom: 30%;
+	position :absolute;
+	width: 350px;
+	height: 380px;
+	margin: 0%;
+}
+
+	h1 { 
+	font-size: 130px;
+	color : white;
+	font-family: Georgia, serif;
+	margin-top: 200px;
+	margin-left: 200px;
+}
+</style> 
