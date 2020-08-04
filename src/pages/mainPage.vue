0bfe84c645f40c03466f99201d6075fd4a1619da
BlakeKim <59683657+BlakeKim@users.noreply.github.com>
2020-08-04 15:55:10 +0900

    Modify mainPage
    Add fullpage ref

--- src/pages/mainPage.vue
+++ src/pages/mainPage.vue
@@ -1,21 +1,26 @@
 <template>
+  <full-page ref="fullpage" :options="options" id="fullpage">
   <div id="main">
-    <section>
-      <AdVue></AdVue>
-    </section>
-    <section>
-      <KoreanVue></KoreanVue>
-    </section>
-    <section>
-      <JapanVue></JapanVue>
-    </section>
-    <section style="background-color: rgb(217, 61, 74);">
-      <ChinaVue></ChinaVue>
-    </section>
-    <section>
-      <AmericaVue></AmericaVue>
-    </section>
-  </div>
+      <div class="section">
+        <AdVue></AdVue>
+      </div>
+      <div class="section">
+        <KoreanVue></KoreanVue>
+      </div>
+      <div class="section">
+        <JapanVue></JapanVue>
+      </div>
+      <div class="section">
+        <ChinaVue></ChinaVue>
+      </div>
+      <div class="section">
+        <AmericaVue></AmericaVue>
+      </div>
+      <div class="section">
+        <FooterVue></FooterVue>
+      </div>
+    </div>
+  </full-page>
 </template>
 
 <script>
@@ -24,12 +29,15 @@ import KoreanVue from '../components/Korea.vue';
 import JapanVue from '../components/Japan.vue';
 import ChinaVue from '../components/Chinese.vue';
 import AmericaVue from '../components/America.vue';
+import FooterVue from '../components/footer.vue';
 
 export default {
   data(){
     return{
       headerFlag : false,
-      tableFlag : true
+      tableFlag : true,
+      scrollbars : true,
+
     }
   },
   components:{
@@ -37,7 +45,8 @@ export default {
     'KoreanVue':KoreanVue,
     'JapanVue':JapanVue,
     'ChinaVue':ChinaVue,
-    'AmericaVue':AmericaVue
+    'AmericaVue':AmericaVue,
+    'FooterVue':FooterVue,
   }
 }
 
@@ -46,23 +55,11 @@ export default {
 <style>
 #main{
   height:100vh;
-  overflow: auto;
-  scroll-snap-type: y mandatory;
-  -ms-scroll-snap-type: mandatory;
 }
 
 body{
   margin:0;
   padding:0;
+  overflow-x:hidden;
 }
-
-#main>section{
-  height:100%;
-  scroll-snap-align:start;
-  -ms-scroll-snap-coordinate:0 0;
-}
-
-
-
-
 </style>
