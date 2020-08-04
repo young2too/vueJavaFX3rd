07ea8644176a1ae141e371c991c056dfabf04b28
young2too <young2too2@naver.com>
2020-08-04 21:30:56 +0900

    화요일 폰트 커밋

--- src/components/Date/Tuesday.vue
+++ src/components/Date/Tuesday.vue
@@ -1,61 +1,59 @@
 <template>
-	<div id="tuesdayPage">
-		<div class="tuesdayFont">
-			<span id="bigWord"><strong>화</strong></span>
-			<span id="firstWord">나서</span>
-			<p id="secondWord">뛰쳐가고 싶은날.</p>
-			<p id="thirdWord">오늘 냉면 한 그릇?</p>
-			<p id="fourthWord">#후루룩#원샷#머리가띵#돈까스는디저트</p>
-		</div>
-	</div>
+   <div id="tuesdayPage">
+      <div class="tuesdayFont">
+         <span id="bigWord">화</span>
+         <span id="firstWord">나서</span><br>
+         <span id="secondWord">뛰쳐가고 싶은 날.</span><br>
+         <span id="thirdWord">오늘 냉면 한 그릇?</span><br>
+         <!-- <span id="fourthWord">#후루룩후루룩#원샷#머리가띵#</span> -->
+      </div>
+   </div>
 </template>
 
 <script>
 export default {
 
-	methods: {
-	}
+   methods: {
+   }
+   
 }
 </script>
 <style scoped>
-	p{
-		margin: 0;
-		padding: 0;
-	}
-	#tuesdayPage{
-		width: 100%;
-		height: 100%;
-		background-size: 100% 100%;
-		position: absolute;
-		background-image: url("../../assets/date_Img/tuesday.jpg");
-		background-repeat: no-repeat;
-		opacity: 0.8;
-  	}
-	.tuesdayFont{
-		margin-top: 150px;
-		margin-left: 150px;
-		color:white;
-		background-position:left;
-	}
-	#bigWord{
-		font-size: 8rem;
-	}
-	#firstWord {
-		font-size: 6rem;
-		/* font-family: 'Times New Roman', Times, serif; */
-	}
-	#secondWord{
-		font-size: 6rem; 
-		/* font-family: 'Times New Roman', Times, serif; */
-	}
-	#thirdWord{
-		font-size: 6rem; 
-		/* font-family: 'Times New Roman', Times, serif; */
-	}
-	#fourthWord{
-		font-size:2.2rem;
-		letter-spacing: 5px;
-		word-spacing: 5px;
-		font-family: 'Times New Roman', Times, serif;
-	}
+   span{
+      line-height: 6rem;
+   }
+   #tuesdayPage{
+      width: 100%;
+      height: 100%;
+      background-size: 100% 100%;
+      position: absolute;
+      background-image: url("../../assets/date_Img/tuesday.jpg");
+      background-repeat: no-repeat;
+     }
+   .tuesdayFont{
+      margin-top: 15%;
+      margin-left: 10%;
+      color:white;
+      background-position:left;
+      font-family: 'BMHANNAPro';
+      letter-spacing: 3px;
+   }
+   #bigWord{
+      font-size: 8rem;
+   }
+   #firstWord {
+      font-size: 4rem;
+      
+   }
+   #secondWord{
+      font-size: 4rem; 
+   }
+   #thirdWord{
+      font-size: 4rem;
+   }
+   #fourthWord{
+      /* margin:7px; */
+      font-size:2rem;
+      /* word-spacing: 20px; */
+   }
 </style>
