c4d6ccb39cdd34182d912ebc326c4f3a880293eb
JungDongJun <67408998+JungDongJun@users.noreply.github.com>
2020-08-04 19:58:43 +0900

    맨위로 고정버튼 추가
    맨위로 고정버튼 추가

---
+++ src/components/up.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="up">
+			<a href="/"><img id="first" src='../assets/footerImg/up.png'></a>
+		</div>
+</template>
+
+<script>
+export default {
+	 
+}
+</script>
+
+<style scoped>
+/* #up{
+	display:scroll;
+	position:fixed;
+	bottom:10px;
+	right:10px;
+} */
+#first{
+	width:60px;
+	height:60px;
+}
+</style>
