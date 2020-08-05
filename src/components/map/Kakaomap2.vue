88ee644bf749239831e696bb382e372e7b72867b
yyjooooo <67408847+yyjooooo@users.noreply.github.com>
2020-08-05 20:44:07 +0900

    지도 메뉴칸 흰색

--- src/components/map/Kakaomap2.vue
+++ src/components/map/Kakaomap2.vue
@@ -301,7 +301,7 @@ export default {
 .map_wrap, .map_wrap * {margin:0;padding:0;font-family:'Malgun Gothic',dotum,'돋움',sans-serif;font-size:12px;}
 .map_wrap a, .map_wrap a:hover, .map_wrap a:active{color:#000;text-decoration: none;}
 .map_wrap {position:relative;width:130vh;height:80vh;margin-left:15%;}
-#menu_wrap {position:absolute;top:0;left:0;bottom:0;width:250px;margin:10px 0 30px 10px;padding:5px;overflow-y:auto;background:rgba(255,255,255,0.3);z-index: 1;font-size:12px;border-radius: 10px;margin-left:130vh}
+#menu_wrap {position:absolute;top:0;left:0;bottom:0;width:250px;margin:10px 0 30px 10px;padding:5px;overflow-y:auto;background: #ffffff;z-index: 1;font-size:12px;border-radius: 10px;margin-left:130vh}
 .bg_white {background:#fff;}
 #menu_wrap hr {display: block; height: 1px;border: 0; border-top: 2px solid #5F5F5F;margin:3px 0;}
 #menu_wrap .option{text-align: center;}
