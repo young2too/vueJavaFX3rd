7d537bc315190d797e3c213405d0bf11ca814966
JungDongJun <67408998+JungDongJun@users.noreply.github.com>
2020-08-05 20:58:48 +0900

    배달vue 생성
    배달vue 생성

---
+++ src/components/baedalBtn.vue
@@ -0,0 +1,39 @@
+<template>
+	<div id="sosBtn">
+		<a href='http://www.bdtong.co.kr'>
+		<img id="baedalImg" src='../assets/header_Img/baedal.png'>
+		</a>
+		<a href='https://www.baemin.com/'>
+		<img id="baeminImg" src='../assets/header_Img/baemin.png'>
+		</a>
+		<a href='http://www.yogiyo.co.kr'>
+		<img id="yogiImg" src='../assets/header_Img/yogiyo.png'>
+		</a>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style scoped>
+#sosBtn{
+	display:inline-block;
+	margin-left:45%;
+}
+#baedalImg{
+	width:60px;
+	height:60px;
+}
+#baeminImg{
+	width:60px;
+	height:60px;
+}
+#yogiImg{
+	width:60px;
+	height:60px;
+}
+</style>
