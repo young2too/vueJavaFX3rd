6b1d631676fb3cd04b54b331121ca72021213840
OH-HYEWON <62108525+OH-HYEWON@users.noreply.github.com>
2020-08-05 16:29:17 +0900

    china

--- src/components/Chinese.vue
+++ src/components/Chinese.vue
@@ -1,11 +1,17 @@
 <template>
    <div class="china">
-      <img id="chaImg" src="../assets/footerImg/chinese1.png" >
-
+      <div id="text">
+      <p id="first">니취팔러마?</p>
+      <p id="second">你吃饭了吗？</p>
+      <p id="third">밥 먹었니?
+         <img id="black" src="../assets/footerImg/black.png" />
+      </p>
+      
       <!-- <a class="Btn" href="#">
         <img src="../assets/footerImg/noBtn.png" />
         <img src="../assets/footerImg/yesbtn.png" />
     </a> -->
+    </div>
    </div>
 </template>
 
@@ -26,9 +32,46 @@ export default {
    height:100%;
 }
 
-#chaImg{
-   margin-left:300px;
+
+#first{
+      margin-top:200px;
+		margin-left: 200px;
+		color:white;
+		background-position:left;
+		font-size: 8rem;
+		font-family: 'BMHANNAPro';
+      text-decoration: underline;
+
+		
+	}
+#second{
+		color:white;
+		background-position:left;
+		font-size: 6rem;
+		font-family: sans-serif;
+      margin-left:200px;
+      margin-top:5px;
+
+		
+	}
+#third{
+      margin-top:5px;
+		margin-left: 200px;
+		color:white;
+		background-position:left;
+		font-size: 6rem;
+		font-family: 'BMHANNAPro';
+
+	}
+p{
+		margin: 0;
+		padding: 0;
+		line-height: 1.8ch;
+	}
+#black{
+   width:400px;
+   height:400px;
+   margin-left: 250px;
+  position:absolute;
 }
-
-
 </style>
