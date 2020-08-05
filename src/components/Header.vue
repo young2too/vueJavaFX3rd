c968498b3111abfe72347fb4d829e2de88ad019a
JungDongJun <67408998+JungDongJun@users.noreply.github.com>
2020-08-05 20:57:12 +0900

    배달버튼 추가
    배달버튼 추가

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,24 +2,25 @@
 	<div class="headerWrapper">
 		<div class="header">
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-			<nav>
 				<h1>
 				<KGImg></KGImg>
 				오늘 뭐먹지
+				<baedalImg></baedalImg>
 				<backImg></backImg>
 				</h1>
-			</nav>
 		</div>
 	</div>	
 </template>
 
 <script>
 import KGImg from '../components/KGBtn.vue';
+import baedalImg from '../components/baedalBtn.vue';
 import backImg from '../components/backBtn.vue';
 
 export default {
 	components:{
 	'KGImg':KGImg,
+	'baedalImg':baedalImg,
 	'backImg':backImg
   }
 }
@@ -35,7 +36,7 @@ export default {
 .header{
 	width:100%;
 	height: 100%;
-	padding: 1rem;
+/* 	padding: 1rem; */
 	color: white;
 	font-weight: bold;
 	display: flex;
@@ -43,11 +44,18 @@ export default {
 	align-items: center;
 	
 }
-h1, h2 {
+h1{
   font-weight: normal;
   font-family: 'BMHANNAPro';
   font-size: 3rem;
+  width:100%;
 }
+h2{
+	font-weight: normal;
+	font-family: 'BMHANNAPro';
+	font-size: 3rem;
+}
+
 
 a {
   color: #42b983;
